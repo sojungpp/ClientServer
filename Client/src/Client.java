ecb2790c8cba3cd23c65ed8c4218e7f1c42d8bfc
sojungpp <sophie2458@naver.com>
2022-10-16 22:40:44 +0900

    Refactor: 중복 코드 제거

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -5,59 +5,52 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public class Client {
 
 	public static void main(String[] args) throws NotBoundException, IOException{
 		ServerIF server;
 		BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			try {
+		try {
 				server = (ServerIF)Naming.lookup("Server");
+			while(true) {
 				showMenu();
 				String userConsoleInput = inputReader.readLine().trim();
-				if (userConsoleInput.equals("1")) showData(server, "student");
-				else if (userConsoleInput.equals("2")) showData(server, "course");
-				else if (userConsoleInput.equals("3")) exitProgram();
-				else if (userConsoleInput.isBlank()) showBlankError();
-				else showEtcError();
-			} catch (RemoteException e) {
-				e.printStackTrace();
+				switch(userConsoleInput) {
+				case "1" :
+					showData(server.getAllStudentData());
+					break;
+				case "2" :
+					showData(server.getAllCourseData());
+					break;
+				case "x":
+					return;
+				default:
+					showEtcError();
+				}
 			}
-		}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			}
 	}
 
-	private static void showCourseData(ServerIF server) throws RemoteException {
-		System.out.println("Server's answer: " + server.getAllCourseData());
+	private static void showData(ArrayList<?> dataList) {
+		String list = "";
+		for(int i=0; i<dataList.size(); i++) {
+			list += dataList.get(i) + "\n";
+		} System.out.println(list);;
 	}
 
 	private static void showEtcError() {
 		System.out.println("It does not exist ! Please try again !");
 	}
 
-	private static void showBlankError() {
-		System.out.println("No value entered. Please try again !");
-	}
-
-	private static void showStudentData(ServerIF server) throws RemoteException {
-		System.out.println("Server's answer: " + server.getAllStudentData());
-	}
-
-	private static void showData(ServerIF server, String string) throws RemoteException {
-		if(string.equals("student")) System.out.println(server.getAllStudentData());
-		else System.out.println(server.getAllCourseData());
-	}
-
-	private static void exitProgram() {
-		System.out.println("~ Bye ~");
-		System.exit(0);
-	}
-
 	private static void showMenu() {
 		System.out.println("\n *********************** MENU ***********************");
 		System.out.println("1. List Students");
 		System.out.println("2. List courses");
-		System.out.println("3. Exit");
+		System.out.println("x. Exit");
 	}
 
 }
